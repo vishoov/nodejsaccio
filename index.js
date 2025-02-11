1318c9d76d75bbf65f4af093e1c20030e50383ec
Vishoo <vverma971@gmail.com>
2025-02-11 22:26:22 +0530

    notes

--- index.js
+++ index.js
@@ -21,6 +21,48 @@ app.get('/', (req, res)=>{
         res.send('<h1>About Page</h1><br><p>This is the about page</p>');
     });
 
+    app.post("/signup", (req, res)=>{
+        const user = req.body;
+        console.log(user);
+        //signup logic
+        res.send("User signed up successfully"); 
+    });
+
+
+
+
+    //dynamic routing
+    
+    //route parameters
+    app.get("/users/:username", (req, res)=>{
+        const username = req.params.username;
+        res.send(`Welcome ${username}`);
+    })
+
+    //Query Parameters
+    app.get("/search", (req, res)=>{
+        const query = req.query;
+        console.log(query);
+        res.send("Search results");
+        //searching logic in the database 
+    })
+    
+    
+
+
+    // HTTP - req and response pair
+
+    // Req -> "about" -> app.get('/about', (req, res))
+        // req -> req.header, req.body, req.params, req.query
+        // res -> res.send(), res.json(), res.render(), res.redirect()
+
+        // get, /cars -> 
+                // json -> Headers -> technical details, auth, type, encoding, etc, 
+                //          Body -> data, content  
+
+    app.get("/contact", (req, res)=>{
+        res.send('<h1>Contact Page</h1><br><p>This is the contact page</p>');
+    })
 
 
 //browsers can only implement get route on the server 
